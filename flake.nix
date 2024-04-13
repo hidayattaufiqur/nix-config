862d22d3e28ddb6cb6be1ad3636a59fc612d5ac9
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-04-14 04:36:34 +0700

    feat: add gpu configs for desktop (inspired by Mustafa's config)

--- flake.nix
+++ flake.nix
@@ -3,13 +3,30 @@ description = "flakes configuration v1";
 
 inputs = {
   nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   home-manager.url = "github:nix-community/home-manager/release-23.11";
 };
 
-outputs = { self, home-manager, nixpkgs }@inputs:
+outputs = { self, home-manager, nixpkgs, nixpkgs-unstable }@inputs:
   let
     system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+
+    upkgs = import nixpkgs-unstable {
+      inherit system;
+      config = {
+        allowunfree = true;
+      };
+    };
+
     specialArgs = inputs // { inherit system; };
+
     # shared-modules = [
     #   home-manager.nixosModules.home-manager
     #   {
@@ -42,17 +59,18 @@ outputs = { self, home-manager, nixpkgs }@inputs:
           }
         ];
       };
-       nixos-box = nixpkgs.lib.nixosSystem {
-        specialArgs = specialArgs;
+
+      nixos-box = nixpkgs.lib.nixosSystem {
+        specialArgs = specialArgs // { inherit upkgs; };
         system = system;
         modules = [
-          ./hosts/desktop/hardware-configuration/default.nix
+          ./hosts/desktop/hardware-configuration/default.nix 
           home-manager.nixosModules.home-manager
           {
             home-manager = {
               useUserPackages = true;
               useGlobalPkgs = true; 
-              extraSpecialArgs = specialArgs;
+              extraSpecialArgs = specialArgs // { inherit upkgs; };
               users.nixos-box = import ./hosts/desktop/home.nix;
             };
           }
