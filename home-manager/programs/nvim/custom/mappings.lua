7b7292aff4a24fca6a057d16d44305587c63adaa
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-04-18 13:46:59 +0700

    feat: aight (for now) nvm nix-way of configuring nvchad, im happy as is.
    also, ive tried to work on this for far too long. need to lock in on the things that more urgent

---
+++ home-manager/programs/nvim/custom/mappings.lua
@@ -0,0 +1,13 @@
+local mapping = require('core.mappings')
+
+-- mapping.copilot = {
+--   i = {
+--    ["<C-space>"] = {
+--       function()
+--         vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+--       end,
+--       "Copilot Accept",
+--       {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+--     }
+--   }
+-- }
