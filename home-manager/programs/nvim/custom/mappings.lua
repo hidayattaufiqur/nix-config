dc85f257caecd495db729a2cf8b391cc7a9bd21e
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-04-22 16:52:19 +0700

    chore: comment out unused nvim config

--- home-manager/programs/nvim/custom/mappings.lua
+++ home-manager/programs/nvim/custom/mappings.lua
@@ -1,15 +1,15 @@
-local mapping = require('core.mappings')
-
-mapping.copilot = {
-  i = {
-   ["<C-space>"] = {
-      function()
-        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-      end,
-      "Copilot Accept",
-      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
-    }
-  }
-}
-
-return mapping
+-- local mapping = require('core.mappings')
+--
+-- mapping.copilot = {
+--   i = {
+--    ["<C-space>"] = {
+--       function()
+--         vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+--       end,
+--       "Copilot Accept",
+--       {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+--     }
+--   }
+-- }
+--
+-- return mapping
