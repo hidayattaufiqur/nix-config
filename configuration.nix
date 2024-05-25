11d26f01bee9513a81e911ca3061109ccf4d76f0
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-05-25 18:13:05 +0700

    chore: rearrange substituter

--- configuration.nix
+++ configuration.nix
@@ -235,7 +235,7 @@
    settings.keep-outputs = "true";
    settings.keep-derivations = "true";
    settings.trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
-   settings.substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" "https://cache.komunix.org/" ];
+   settings.substituters = [ "https://cache.nixos.org" "https://nix-community.cachix.org" "https://cache.komunix.org/" ];
   }; 
 
   boot.supportedFilesystems = [ "ntfs" ];
