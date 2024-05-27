b20f191389e0975ad65dcbab68abedbf490ed2bb
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-05-27 17:49:07 +0700

    feat: add disk-config for disko

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -183,7 +183,8 @@
 
    settings.keep-outputs = "true";
    settings.keep-derivations = "true";
-   settings.substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" "https://cache.komunix.org/" ];
+   settings.trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+   settings.substituters = [ "https://cache.nixos.org" "https://nix-community.cachix.org" "https://cache.komunix.org/" ];
   }; 
 
   boot.supportedFilesystems = [ "ntfs" ];
