923ef78f88dc756cf00c18e0176268d73765a197
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-06-05 14:00:16 +0700

    feat(nginx): add proxy headers to set the content-type for flask app

--- hosts/server/services/nginx.nix
+++ hosts/server/services/nginx.nix
@@ -17,7 +17,11 @@ in
         locations."/" = {
           proxyPass = "http://127.0.0.1:5000";
           extraConfig = ''
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Forwarded-Host $host;
+            proxy_set_header X-Forwarded-Prefix /;
+            proxy_set_header Content-Type application/json;
           '';
         };
       };
