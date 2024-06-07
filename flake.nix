75fcc9352e826e581a934b8c075a6861c342b7ef
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-06-07 14:53:31 +0700

    feat(flake): upgrade to 24.05

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
-description = "flakes configuration v1";
+description = "drunkwhales' personal flake configuration";
 
 inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  nixpkgs-prev.url = "github:NixOS/nixpkgs/nixos-23.11";
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  home-manager.url = "github:nix-community/home-manager/release-23.11";
+  home-manager.url = "github:nix-community/home-manager";
+  home-manager.inputs.nixpkgs.follows = "nixpkgs";
   sops-nix.url = "github:Mic92/sops-nix";
   nur.url = "github:nix-community/NUR";
 };
