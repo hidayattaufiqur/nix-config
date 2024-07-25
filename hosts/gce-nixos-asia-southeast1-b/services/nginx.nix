043fda5f77f9297a0f7195ea258702928c66ff27
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-07-25 17:25:02 +0700

    perf(ta-sg): optimize nginx configurations

--- hosts/gce-nixos-asia-southeast1-b/services/nginx.nix
+++ hosts/gce-nixos-asia-southeast1-b/services/nginx.nix
@@ -6,7 +6,12 @@
   services.nginx = {
     enable = true;
     package = pkgs.nginxQuic; # for HTTP/3
-    eventsConfig = "worker_connections 1024;";
+    eventsConfig = "worker_connections 2048;";
+    recommendedProxySettings = true;
+    recommendedGzipSettings = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    logError = "stderr debug";
 
     virtualHosts = {
       "ta-sg.hidayattaufiqur.dev" = {
@@ -14,15 +19,10 @@
         forceSSL = true;
         enableACME = true;
 
-        ## HTTP/1.1
-        # enableACME = true;
-
         ## HTTP/2
         # http2 = true;
-        # enableACME = true;
-        # forceSSL = true;
 
-        ## HTTP/http3
+        ## HTTP/3
         quic = true;
         http3 = true;
         reuseport = true;
@@ -33,9 +33,15 @@
           proxyPass = "http://localhost:5000";
           extraConfig = ''
             add_header X-protocol $server_protocol always;
-            add_header Alt-Svc  'h3=":$server_port"; ma=3600, h2=":$server_port"; ma=3600';
+            add_header Alt-Svc 'h3-23=":443"; ma=3600, h2=":443"; ma=3600';
             add_header x-quic 'h3';
             add_header Cache-Control "no-cache,no_store"; 
+
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-protocol $server_protocol;
           '';
         };
       };
