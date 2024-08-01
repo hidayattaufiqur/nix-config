cdf2c60f43137a5a2c2eafb41c204b77d8bd2aca
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-08-01 19:07:32 +0700

    feat(nvim): add noice as neovim wildmenu (command pallete) replacement

--- home-manager/programs/nvim/custom/plugins.lua
+++ home-manager/programs/nvim/custom/plugins.lua
@@ -1,6 +1,22 @@
 ---@diagnostic disable: different-requires
 local plugins = {
   {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      local opts = require("custom.configs.noice").opts
+      require("noice").setup(opts)
+    end,
+  },
+  {
     "kdheepak/lazygit.nvim",
       cmd = {
       "LazyGit",
