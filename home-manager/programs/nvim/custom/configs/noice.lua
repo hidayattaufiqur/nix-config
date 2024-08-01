5a95a54fbba295a2e2ad507d67e667cc6320adc7
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-08-01 19:16:08 +0700

    feat(nvim): disable hover and signature features for Noice

--- home-manager/programs/nvim/custom/configs/noice.lua
+++ home-manager/programs/nvim/custom/configs/noice.lua
@@ -1,4 +1,4 @@
-local M = {} 
+local M = {}
 
 M.opts = {
   lsp = {
@@ -7,6 +7,23 @@ M.opts = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
       ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+    hover = {
+      enabled = false,
+      silent = false, -- set to true to not show a message if hover is not available
+      view = nil, -- when nil, use defaults from documentation
+      opts = {}, -- merged with defaults from documentation
+    },
+    signature = {
+      enabled = false,
+      auto_open = {
+        enabled = true,
+        trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+        luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+        throttle = 50, -- Debounce lsp signature help request by 50ms
+      },
+      view = nil, -- when nil, use defaults from documentation
+      opts = {}, -- merged with defaults from documentation
     },
   },
   -- you can enable a preset for easier configuration
