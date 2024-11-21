1807ba70485942ba2ff9eb03a01e53ba249b06b2
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-11-21 15:54:28 +0700

    fix(rofi): fonts not found

--- home-manager/programs/rofi/theme.nix
+++ home-manager/programs/rofi/theme.nix
@@ -1,5 +1,6 @@
 {
   xdg.configFile."rofi/onedark.rasi".source = ./style-10.rasi;
   xdg.configFile."rofi/colors/onedark.rasi".source = ./colors/onedark.rasi;
+  xdg.configFile."rofi/fonts.rasi".source = ./fonts.rasi;
 }
 
