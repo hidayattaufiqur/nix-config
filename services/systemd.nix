3cf6a20855e90d56f81365f71574883274dbf702
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2025-01-12 03:42:53 +0700

    feat(server): comment out unused nginx virtual hosts

--- services/systemd.nix
+++ services/systemd.nix
@@ -16,29 +16,29 @@ in
       Group = "users";
       WorkingDirectory = ontology-be;
       ExecStart = "/home/nixos-server/Fun/Projects/ontology-BE/.venv/bin/gunicorn --config gunicorn.conf.py app.__init__:'create_app()'";
-      # Restart = "always";
-      StandardOutput = "journal";
-      StandardError = "journal";
+      Restart = "always";
+      StandardOutput = "null";
+      StandardError = "null";
       Environment = [ "PATH=/home/nixos-server/Fun/Projects/ontology-BE/.venv/bin" "LD_LIBRARY_PATH=${pkgs.libstdcxx5.out}/lib:${pkgs.stdenv.cc.cc.lib}/lib" ];
     };
   };
 
-  systemd.services.blogablog = {
-    description = "systemd unit to run blogablog";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      User = "nixos-server";
-      Group = "users";
-      WorkingDirectory = blogablog;
-      Environment = [
-        "PATH=/home/nixos-server/.nix-profile/bin:/etc/profiles/per-user/nixos-server/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:/usr/bin"
-      ];
-      ExecStart = "${pkgs.nodejs}/bin/npm start"; 
-      StandardOutput = "journal";
-      StandardError = "journal";
-    };
-  };
+  # systemd.services.blogablog = {
+  #   description = "systemd unit to run blogablog";
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     User = "nixos-server";
+  #     Group = "users";
+  #     WorkingDirectory = blogablog;
+  #     Environment = [
+  #       "PATH=/home/nixos-server/.nix-profile/bin:/etc/profiles/per-user/nixos-server/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:/usr/bin"
+  #     ];
+  #     ExecStart = "${pkgs.nodejs}/bin/npm start"; 
+  #     StandardOutput = "journal";
+  #     StandardError = "journal";
+  #   };
+  # };
     
   systemd.services.hidayattaufiqurDev = {
     description = "systemd unit to run personal site";
@@ -57,22 +57,22 @@ in
     };
   };
 
-  systemd.services.eigen-tc = {
-    description = "systemd unit to run eigen-tc API";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      User = "nixos-server";
-      Group = "users";
-      WorkingDirectory = eigen-tc;
-      Environment = [
-        "PATH=/home/nixos-server/.nix-profile/bin:/etc/profiles/per-user/nixos-server/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:/usr/bin"
-      ];
-      ExecStart = "${pkgs.nodejs}/bin/npm start"; 
-      StandardOutput = "journal";
-      StandardError = "journal";
-    };
-  };
+  # systemd.services.eigen-tc = {
+  #   description = "systemd unit to run eigen-tc API";
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     User = "nixos-server";
+  #     Group = "users";
+  #     WorkingDirectory = eigen-tc;
+  #     Environment = [
+  #       "PATH=/home/nixos-server/.nix-profile/bin:/etc/profiles/per-user/nixos-server/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:/usr/bin"
+  #     ];
+  #     ExecStart = "${pkgs.nodejs}/bin/npm start"; 
+  #     StandardOutput = "journal";
+  #     StandardError = "journal";
+  #   };
+  # };
 
   users.groups.gunicorn.members = [ "nginx" "nixos-server" ];
 }
